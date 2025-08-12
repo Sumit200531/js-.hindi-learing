36df7b93d25516e03c76a2509b4a375c18d42192
sumit goswami <142250806+Sumit200531@users.noreply.github.com>
2025-08-12 16:36:42 +0000

    day one completed for klearing javasctript

---
+++ 01-basics/01-varibales.js
@@ -0,0 +1,18 @@
+const account_Id = 2435  // value ko  no changes
+let accountEmail  = "sumtigowwami9001@gmail.com" // 
+var accountPassword = "1323"
+accountCity = "jaipur"
+let practicevalue;
+//account_Id  = 5
+accountEmail = 'oq1323@gmail.com'
+accountPassword = 2635
+//accountCity =  'citynow'
+console.log(account_Id);
+
+/* 
+ var mostly use nhi karte , as ussue in block scope and funcntional 
+ scope
+ bina variable declare kare bhi kar skthe h , but ye achi addat nhi h 
+
+*/
+console.table([accountEmail, accountPassword, account_Id, accountCity, practicevalue])
