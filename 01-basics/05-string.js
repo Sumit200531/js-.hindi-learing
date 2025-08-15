3bb52b0748831b124dfcedf6b0446e0ab3ea6111
sumit goswami <142250806+Sumit200531@users.noreply.github.com>
2025-08-15 12:52:29 +0000

    string topic

---
+++ 01-basics/05-string.js
@@ -0,0 +1,8 @@
+const  name = "Sumit" 
+const repo_conunt  =23
+
+console.log(name + repo_conunt + "value")
+//very outdated instead we can use the new inject value  that is ${}
+
+console.log(`hello my name is ${name} and my repo count is ${repo_conunt} thnak you for learing`)
+
