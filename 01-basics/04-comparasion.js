8fdd7818979788766d6b289a7cfd86bedc884ed8
sumit goswami <142250806+Sumit200531@users.noreply.github.com>
2025-08-15 11:52:24 +0000

    ccomparasion

---
+++ 01-basics/04-comparasion.js
@@ -0,0 +1,23 @@
+//console.log(3<1)
+
+//console.log(1>0)
+
+//console.log("2" > 1)
+///console.log("01" > 1)
+
+
+console.log(null >0)//false 
+
+console.log(null <0)//false
+
+
+console.log(null >=0) //true
+
+
+console.log(null <=0) //true
+
+console.log(null ==0) //false
+
+
+// strict check -> ===
+console.log("2"===2)
