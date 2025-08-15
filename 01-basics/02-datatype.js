4d1a20c74b9f804588e167f4d62b1e02ad2afdc4
sumit goswami <142250806+Sumit200531@users.noreply.github.com>
2025-08-15 10:55:02 +0000

    datatypes

---
+++ 01-basics/02-datatype.js
@@ -0,0 +1,26 @@
+"use strict";// treat all JS code as newer version
+
+//alert(4+4) //we are using nodejs, not browser
+
+console.log(4+5)
+
+console.log("datatpye")
+
+
+let name = " efs "
+let age = 34
+let state;
+let isloggegin = true
+
+//number -> 2^53
+//bigint-> for large numbers
+// string  -> ""
+// boolean -> true/false
+//null-> stand alone value
+// undefined -> 
+//symbol -> unique 
+
+
+// object 
+
+console.log(typeof null) // object
