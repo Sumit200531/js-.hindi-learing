ac4404bc7b4b3afc583ed0cebd5b7709ec5bfe64
sumit goswami <142250806+Sumit200531@users.noreply.github.com>
2025-08-15 12:19:36 +0000

    datattypes all

---
+++ 01-basics/datatype-summary.js
@@ -0,0 +1,40 @@
+// #primitive 
+
+// 7 categories : String , Number , Boolean , null, undefined , Symbol,BigInt
+
+const vale  = 232 
+
+const scroe = 230.52
+
+let outside = false
+let outs = null
+let useremail
+
+const id =Symbol("2")
+const anoteherid = Symbol("2")
+
+console.log(id== anoteherid) // false
+
+//const bignumver = 345465476345345n
+
+
+// reference type(non-primtive) 
+
+// Array , Ojects, Functions
+
+const herp = ["a", "b", "c"]; // araray - eturn type object ata h
+// 
+
+
+
+
+
+let car = { model : "size",  year : 23 , exp : "yoe2"} // return type  oject h
+
+
+const myfunction = function(){ // return type function ata hoga
+    console.log("hello wrld")
+}
+
+
+console.log(typeof car)
