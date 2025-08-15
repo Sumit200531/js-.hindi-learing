c9b4f06025cd438c537bdc766c615bb8f0ecd4b9
sumit goswami <142250806+Sumit200531@users.noreply.github.com>
2025-08-15 11:28:35 +0000

    operations

---
+++ 01-basics/03-conversion.js
@@ -0,0 +1,50 @@
+let score = "kroya"
+
+//console.log(typeof score)
+
+let calueinnumber = Number(score)
+//console.log(typeof calueinnumber)
+//console.log(calueinnumber) // not a number ->NaN
+/* 
+""33" -> 33
+"33abc" -> Nan
+true->1 ; false = 0
+*/
+let isloggedin = ""
+
+let boleinlogginin = Boolean(isloggedin)
+//console.log(boleinlogginin)
+/* 1->true
+   0->false
+   "" -> false
+   "any string" -> true
+   */
+
+   let somenumber = 34;
+   let newvalue = String(somenumber)
+   //console.log(somenumber)
+  // console.log(typeof newvalue)
+   
+//****************************operation **********************
+
+
+let value = 43
+let negvalue = -value
+
+let str1 = "hello"
+let str2 = "et wf"
+let str3 =  str1+ str2
+//console.log(str3)
+
+
+//console.log(1 + "4")
+//console.log("1" + 45+ 2)
+//console.log(1 + 435 + "2")
+
+//console.log(+false)-> 0
+
+
+let gamecouner  = 100
+
+++gamecouner
+console.log(gamecouner) // -> 101
