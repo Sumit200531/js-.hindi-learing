f50d23f6d6cabd2787d1f611a6b8ce0bda93261d
sumit goswami <142250806+Sumit200531@users.noreply.github.com>
2025-08-18 16:31:45 +0000

    array 2

--- 02-basics/02-array.js
+++ 02-basics/02-array.js
@@ -1,3 +1,22 @@
 const romcom = ["horiyama", "hoyuka" ,"kagyua-sama"]
 const fun  = ["Aot", "demon slayer", "naruto"]
 
+//romcom.push(fun)
+// console.log(romcom)
+ //   'horiyama',
+ //   'hoyuka',
+//   'kagyua-sama',
+ //   [ 'Aot', 'demon slayer', 'naruto' ]
+ // ]
+
+// console.log(romcom[3][1])
+
+//  const a = romcom.concat(fun)
+// console.log(a)
+
+
+const allnw = [...romcom, ...fun]
+console.log(allnw)
+
+
+
