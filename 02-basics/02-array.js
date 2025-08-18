c2b08a8835ddf3c54d864b45bc5b95c2067b4392
sumit goswami <142250806+Sumit200531@users.noreply.github.com>
2025-08-18 18:02:44 +0000

    array 2

--- 02-basics/02-array.js
+++ 02-basics/02-array.js
@@ -15,8 +15,32 @@ const fun  = ["Aot", "demon slayer", "naruto"]
 // console.log(a)
 
 
-const allnw = [...romcom, ...fun]
-console.log(allnw)
+const allnw = [...romcom, ...fun] // SPREAD
+// console.log(allnw)
+// [
+//   'horiyama',
+//   'hoyuka',
+//   'kagyua-sama',
+//   'Aot',
+//   'demon slayer',
+//   'naruto'
+// ]
 
+// const w = [24,5,3,[53,65], 4, 6, [6.8,[5, 67]]]
+// const real = w.flat(Infinity)
+// console.log(real)
+//  ->
+// [
+//   24, 5,   3, 53, 65,
+//    4, 6, 6.8,  5, 67
+// ]
 
+// console.log(Array.isArray(allnw)) //true
+// console.log(Array.from("sumit goswami")) // [s,u.....]
 
+// console.log(Array.from({name : "sumt"}))
+// []
+
+let c=32 ,o=35,p=64
+
+console.log(Array.of(c,o,p)) //[ 32, 35, 64 ]
