e29a4f9ada048bc98b00eb87bbfd3eb5002ae895
sumit goswami <142250806+Sumit200531@users.noreply.github.com>
2025-08-18 09:44:36 +0000

    math and number

--- 01-basics/06-NUmberandmath.js
+++ 01-basics/06-NUmberandmath.js
@@ -5,11 +5,36 @@ const va = new String(100000)
 
 
 
-console.log(bal.toString().length)//3
-console.log(va.length)//6
-console.log(bal.toFixed(2)) // 100.00 -> precision value
+// console.log(bal.toString().length)//3
+// console.log(va.length)//6
+// console.log(bal.toFixed(2)) // 100.00 -> precision value
 
-//toPrecision -> precise value most before decimal values
 let othervalue = 13546.547
-console.log(othervalue.toPrecision(3)) //1.35e+4
-console.log(othervalue.toPrecision(5)) //13547 
+// console.log(othervalue.toPrecision(3)) //1.35e+4
+// console.log(othervalue.toPrecision(5)) //13547 
+
+const hundredds = 1000000
+// console.log(hundredds.toLocaleString('en-IN')) //10,00,000
+
+//****************maths******************* */
+
+console.log(Math)
+console.log(Math.abs(-4)) // 4
+console.log(Math.round(6.3)) //6
+
+console.log(Math.ceil(4.3))//5
+console.log(Math.floor(5.8))//5
+
+console.log(Math.min(798,5,7,5))
+console.log(Math.max(4,5,0,57))
+
+console.log((Math.random()*10) + 1)
+
+
+const min = 10
+const max = 20
+
+console.log(Math.random() * (max- min + 1))
+
+console.log(Math.floor(Math.random() * (max- min +1))+ min)
+
