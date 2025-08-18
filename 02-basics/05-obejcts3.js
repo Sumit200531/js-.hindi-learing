14ca509f80cbfe752810daebf677b42ba5f09837
sumit goswami <142250806+Sumit200531@users.noreply.github.com>
2025-08-18 19:08:54 +0000

    json , apis intro

---
+++ 02-basics/05-obejcts3.js
@@ -0,0 +1,21 @@
+//destructuering
+
+const course  = {
+    coursename : "btech",
+    orice : 23,
+    year : 2424
+}
+
+// course.orice
+
+const {coursename : c} = course
+
+console.log(c) //btech
+
+
+//APIS
+// {
+//     name : "Sfa",
+//     coursenamerr : "24",
+//     price : "3212"
+// }
