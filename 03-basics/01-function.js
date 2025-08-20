f02e7d5b1867a0af3d5212ef18d5bc4ffa13bae1
sumit goswami <142250806+Sumit200531@users.noreply.github.com>
2025-08-20 11:13:56 +0000

    function-2

--- 03-basics/01-function.js
+++ 03-basics/01-function.js
@@ -6,6 +6,8 @@
 // console.log("i")
 // console.log("t")
 
+const { use } = require("react");
+
 // }
 
 // first();
@@ -42,7 +44,50 @@ function lginuser(username= "no value"){
 
 // lginuser("Wow") // no result as we just calling not printing the res
 
-// console.log(lginuser("sumit")) //sumit just logged in
-console.log(lginuser()) //undefined just logged in
+// // console.log(lginuser("sumit")) //sumit just logged in
+// console.log(lginuser()) //undefined just logged in
 
 
+
+function calculate_cart_price(num1){
+   return num1;
+}
+console.log(calculate_cart_price(4)) //4
+
+
+
+
+function calculate_cart_price_two(...vale){
+   return vale;
+}
+console.log(calculate_cart_price_two(23,56,86,)); //[ 23, 56, 86 ]
+
+const user  = {
+     romcmo : "hyouka",
+     year : 2013,
+     studio : "kyoto animation"
+}
+
+function anime(anyobject){
+   console.log(`the name of anime is ${anyobject.romcmo} and 
+      it is produced in year ${anyobject.year} 
+      also the studio name is ${anyobject.studio}`)
+   }
+
+ anime(user)
+
+// user({
+//    romcmo : "clannad",
+//    year  :23,
+//    studio  : "cmc"
+// })
+
+
+
+const newar = [32,445,35,342]
+
+function returnvalue(newar){
+   return newar[3]
+}
+
+console.log(returnvalue(newar))
