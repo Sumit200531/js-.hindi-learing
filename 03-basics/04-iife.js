9ff739c40fe98689ae2120a3f2d51ef058130308
sumit goswami <142250806+Sumit200531@users.noreply.github.com>
2025-08-21 12:52:36 +0000

    IIFE

--- 03-basics/04-iife.js
+++ 03-basics/04-iife.js
@@ -1,7 +1,25 @@
 // Immediately Invoked Function Expression (IIFE)
 
 
-function chai(){
-    console.log("db connected")
-}
-chai()
+// (function chai(){
+//     console.log("db connected")
+// })()
+//we use this to avoiding global scope pollution
+
+// chai()
+
+ 
+var x =24;
+
+// (function (){
+//    x =42
+//    console.log("iner" , x)
+
+// }) ();
+
+// console.log("outer", x)
+
+
+( () =>   {
+    console.log(`ksfds`);
+}) ();
